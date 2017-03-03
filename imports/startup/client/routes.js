3bb106923065f13f2f24a74c40d164cb3db1a3a4
Sam Fiddis <Sam.Fiddis@gmail.com>
2017-03-03 10:30:07 +0000

    Finally working!

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,6 +1,5 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
-import { AccountsTemplates } from 'meteor/useraccounts:core';
 
 // Import to load these templates
 import '../../ui/pages/homepage.js';
@@ -8,6 +7,8 @@ import '../../ui/pages/mainpage.js';
 import '../../ui/pages/howitworks.js';
 import '../../ui/pages/studentoverview.js';
 import '../../ui/pages/viewthecircle.js';
+import '../../ui/pages/donorregister.js';
+import '../../ui/pages/studentregister.js';
 import '../../ui/components/navBar.js';
 
 
@@ -41,5 +42,19 @@ FlowRouter.route('/viewthecircle', {
   name: 'viewthecircle',
   action(){
     BlazeLayout.render('mainpage', {main: 'viewthecircle'});
+  },
+});
+
+FlowRouter.route('/studentregister', {
+  name: 'studentregister',
+  action(){
+    BlazeLayout.render('mainpage', {main: 'studentregister'});
+  },
+});
+
+FlowRouter.route('/donorregister', {
+  name: 'donorregister',
+  action(){
+    BlazeLayout.render('mainpage', {main: 'donorregister'});
   },
 });
--- imports/ui/pages/donorregister.js
+++ imports/ui/pages/donorregister.js
@@ -1,8 +1,4 @@
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
-import '../components/navBar.js';
-
-import './mainpage.html';
-
-
+import './donorregister.html';
