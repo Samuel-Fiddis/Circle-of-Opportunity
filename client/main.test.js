dc63b85a502a1977015edd9cf15ab0e4e59a34b8
Coline <cmc16@ic.ac.uk>
2017-03-04 15:47:29 +0000

    testing init not working

---
+++ client/main.test.js
@@ -0,0 +1,21 @@
+/*
+describe('my module', function () {
+  beforeEach(function () {
+    resetDatabase();
+  });
+  it('does something that should be tested', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
+  });
+});
+
+*/
+
+describe('Hello', function(){
+  describe('#counter', function(){
+    it('should return -1 when no value or negative ') // placeholder
+    it('should return the value when value is positive', function(){
+      assert.equal(0, hello.counter());
+    })
+  })
+})
