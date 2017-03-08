1e129e7f87db0a91c62270f9a7d14e7bbc079f61
Jack <jack.tanner16@imperial.ac.uk>
2017-03-08 17:55:19 +0000

    stubs for ethereum accounts

--- imports/api/ethereum.test.js
+++ imports/api/ethereum.test.js
@@ -17,9 +17,51 @@ if (!(Meteor.isServer)) {
         });
 
         describe('ethCreateAccount(password)', () => {
-            it('return value > 0', () => {
-                assert.equal(0, 0);
+            it('new file is created in keystore folder', () => {
+              var files_before = 3;
+              ethCreateAccount('password');
+              assert.equal(files_before+1, 4);
+            });
+        });
+
+        describe('ethAllAccounts()', () => {
+            it('array size is same as number of files in keystore folder', () => {
+              var value;
+              value = ethAllAccounts('password');
+              assert.equal(0, 0);
             });
         });
     })
 }
+
+// if (Meteor.isServer) {
+//   describe('ethCreateAccount(password)', () => {
+//       it('return value > 0', () => {
+//         // var exec = require('child_process').exec, child;
+//         //
+//         // child = exec('ls | wc -l',
+//         //   function (error, stdout, stderr) {
+//         //       console.log('stdout: ' + stdout);
+//         //       console.log('stderr: ' + stderr);
+//         //       if (error !== null) {
+//         //            console.log('exec error: ' + error);
+//         //       }
+//         //   });
+//         // var out = child();
+//         // console.log("out:");
+//         // console.log(out);
+//
+//         // var oShell = new ActiveXObject("Shell.Application");
+//         // console.log("oShell");
+//         // console.log(oShell);
+//         // var commandtoRun = "C:\\Winnt\\Notepad.exe";
+//         // if (inputparms != "") {
+//         //   var commandParms = document.Form1.filename.value;
+//         // }
+//         //
+//         // // Invoke the execute method.
+//         // oShell.ShellExecute(commandtoRun, commandParms, "", "open", "1");
+//         //   assert.equal(0, 0);
+//       });
+//   })
+// }
