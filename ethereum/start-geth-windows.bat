ebc1001a4c4a0940603dd7769f60068b0b725a22
Sam Fiddis <Sam.Fiddis@gmail.com>
2017-03-27 14:29:03 +0100

    Added start-get-windows.bat file

---
+++ ethereum/start-geth-windows.bat
@@ -0,0 +1,7 @@
+cd C:\Users\Sam\AppData\Roaming\Ethereum\testnet
+# read -rsp $'Press enter to continue...\n'
+
+# start Geth on testnet blockchain with CORS to allow incoming connections from localhost:3000
+geth --rpc --rpccorsdomain "http://localhost:3000" --rpcapi "db,eth,net,personal,web3" --testnet
+
+pause
