2f76afc2bd385d28da78c6796f04602c73014407
Sam <saf16@ic.ac.uk>
2017-04-07 17:55:59 +0100

    Added Imperial Emails to adminconfig.js file. Signup using your IC email and log in then go to localhost:3000/admin

--- admin/adminconfig.js
+++ admin/adminconfig.js
@@ -1,6 +1,6 @@
 AdminConfig = {
-adminEmails: ['saf16@ic.ac.uk'],
+adminEmails: ['saf16@ic.ac.uk','cmc16@ic.ac.uk','lhg16@ic.ac.uk','ahd216@ic.ac.uk','jnt16@ic.ac.uk', 'cg916@ic.ac.uk'],
 collections:
 {
 }
-} 
+}
