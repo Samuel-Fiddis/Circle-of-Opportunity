d856ae0554e60fc4d0a986de19629ba187b8838a
Coline <cmc16@ic.ac.uk>
2017-04-11 21:34:20 +0200

    run eth command flag --fast

--- ethereum/start-geth-windows.bat
+++ ethereum/start-geth-windows.bat
@@ -2,6 +2,6 @@ cd C:\Users\Sam\AppData\Roaming\Ethereum\testnet
 # read -rsp $'Press enter to continue...\n'
 
 # start Geth on testnet blockchain with CORS to allow incoming connections from localhost:3000
-geth --rpc --rpccorsdomain "http://localhost:3000" --rpcapi "db,eth,net,personal,web3" --testnet
+geth --rpc --rpccorsdomain "http://localhost:3000" --rpcapi "db,eth,net,personal,web3" --testnet --fast
 
 pause
