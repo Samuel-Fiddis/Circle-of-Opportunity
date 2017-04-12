5107c93bc3a9dd98ad33551b586fb949b7cc278e
Coline <cmc16@ic.ac.uk>
2017-04-12 11:10:22 +0200

    publish function

--- imports/api/users/server/publish.js
+++ imports/api/users/server/publish.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 // All variables to publish are kept in the fields option
 Meteor.publish('userData', function () {
-	return Meteor.users.find({userType: "student"},{fields: {'_id': 1,'name': 1,'age': 1, 'bio': 1, 'uni_info': 1, 'address.city': 1, 'address.country': 1}});
+	return Meteor.users.find({userType: "student"},{fields: {'_id': 1,'name': 1,'age': 1, 'bio': 1, 'uni_info': 1, 'address.city': 1, 'address.country': 1 }});
 });
 
 // All variables to publish are kept in the fields option
@@ -12,11 +12,12 @@ Meteor.publish('donorData', function () {
 
 // Private publication for the data of currently logged in user
 Meteor.publish('thisUser', function(userId) {
-	return Meteor.users.find({_id: userId});
+	check(userId, String);
+	return Meteor.users.find({_id: userId}, {fields: {'_id': 1}});
 })
 
 // Publish just one users info depending on their id
 Meteor.publish('singleUser', function (id) {
 	check(id, String);
-	return Meteor.users.find({_id: id},{fields: {'_id': 1,'name': 1,'age': 1, 'bio': 1, 'uni_info': 1, 'address.city': 1, 'address.country': 1}});
+	return Meteor.users.find({_id: id},{fields: {'_id': 1,'name': 1,'age': 1, 'bio': 1, 'uni_info': 1, 'address.city': 1, 'address.country': 1, 'ethereum': 1}});
 });
