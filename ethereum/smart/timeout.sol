7b30ac2f1231015c1b6e4d4fd4a63d6c0eb3caa8
Jack <jack.tanner16@imperial.ac.uk>
2017-05-04 20:36:38 +0100

    added mortal contract code (not inheriting yet, added TODOs

--- ethereum/smart/timeout.sol
+++ ethereum/smart/timeout.sol
@@ -1,9 +1,22 @@
 pragma solidity ^0.4.0;
 
+contract mortal {
+    /* Define variable owner of the type address*/
+    address owner;
+
+    /* this function is executed at initialization and sets the owner of the contract */
+    function mortal() { owner = msg.sender; }
+
+    /* Function to recover the funds on the contract */
+    function kill() { if (msg.sender == owner) selfdestruct(owner); }
+}
+
 contract Timeout {
     mapping (address => uint) storedData;
 
     function set() payable {
+        // TODO: check that sendto address is valid, if not then throw
+
         storedData[msg.sender] = msg.value;
     }
 
