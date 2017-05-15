c8b86c027c012e18c430eb300d114d502464009f
Jack <jack.tanner16@imperial.ac.uk>
2017-05-15 15:30:47 +0100

    Updated Livefeed and made donatenow page only display donations to the general pot.

--- imports/api/transactions/server/publish.js
+++ imports/api/transactions/server/publish.js
@@ -2,7 +2,7 @@ import { Transactions } from '/imports/api/transactions/transactions.js';
 
 // All variables to publish are kept in the fields option
 Meteor.publish('transactions', function () {
-	return Transactions.find({type: "DtS"});
+
 });
 
 Meteor.publish('topTransactions', function (nT) {
@@ -15,4 +15,8 @@ Meteor.publish('topStudentTransactions', function(id,nT){
 
 Meteor.publish('topDonorTransactions', function(id,nT){
 	return Transactions.find({idSender: id},{sort: {createdAt: -1}, limit: nT});
+});
+
+Meteor.publish('topGeneralPotTransactions', function(nT){
+	return Transactions.find({type: "DtG"},{sort: {createdAt: -1}, limit: nT});
 });
