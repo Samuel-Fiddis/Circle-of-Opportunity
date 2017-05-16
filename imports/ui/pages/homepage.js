3e73cc2d77cd7a3d080d69e10c5409e71a3e1467
Corey Garvey <CGarv16@gmail.com>
2017-05-16 16:24:45 +0100

    removed excess lines

--- imports/ui/pages/homepage.js
+++ imports/ui/pages/homepage.js
@@ -5,7 +5,6 @@ import { Universities } from '../../api/universities/universities.js';
 
 import './homepage.html';
 import '../components/liveFeed.js';
-
 //import '../components/ticker.js'
 
 Template.homepage.helpers({
