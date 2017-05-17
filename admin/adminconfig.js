48b243fb46b81d8bb4432d585e7ed8b9b1d7cf66
Jack <jack.tanner16@imperial.ac.uk>
2017-05-17 13:18:04 +0100

    Added uni@uni.uni to admin access. Updated Readme file to detail admin access.

--- admin/adminconfig.js
+++ admin/adminconfig.js
@@ -3,7 +3,7 @@
 AdminConfig = {
 
 // Admin emails
-adminEmails: ['saf16@ic.ac.uk','cmc16@ic.ac.uk','lhg16@ic.ac.uk','ahd216@ic.ac.uk','jnt16@ic.ac.uk', 'cg916@ic.ac.uk'],
+adminEmails: ['saf16@ic.ac.uk','cmc16@ic.ac.uk','lhg16@ic.ac.uk','ahd216@ic.ac.uk','jnt16@ic.ac.uk', 'cg916@ic.ac.uk','uni@uni.uni'],
 
 // Define other collections to show here
 collections:
