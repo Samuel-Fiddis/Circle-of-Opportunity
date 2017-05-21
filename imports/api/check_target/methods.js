8e8939807d3aad1f25a425438433eefabfdf90b4
Audrey Desvaux <audreydesvaux@dyn1221-139.wlan.ic.ac.uk>
2017-05-21 15:28:34 +0100

    Changes profile page to target reached by querying database instead of ethGetBalance, so updates immediatly

--- imports/api/check_target/methods.js
+++ imports/api/check_target/methods.js
@@ -7,7 +7,9 @@ Meteor.methods({
     console.log("checking if target is reached");
     var user = Meteor.users.findOne({_id: studentId});
     // Set up variables to be used
-    var Balance = ethGetBalance(user.ethereum);
+    // var Balance = ethGetBalance(user.ethereum);
+
+    var Balance = Meteor.call('totalDonation',studentId);
     // Need to define how to check the target
     var Target = user.uni_info.tuition_eth;
     Target += user.uni_info.allowance_eth * 10;
