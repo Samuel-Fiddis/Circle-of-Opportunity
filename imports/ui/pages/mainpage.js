9c9f31e0c4ad81732aa10d1d6bedeebddb782524
Coline <cmc16@ic.ac.uk>
2017-05-22 20:12:58 +0100

    banner added for presentation

--- imports/ui/pages/mainpage.js
+++ imports/ui/pages/mainpage.js
@@ -6,4 +6,31 @@ import '../components/footBar.js';
 
 import './mainpage.html';
 
+Template.mainpage.onCreated( function () {
 
+  var self = this;
+  self.autorun(function() {
+    var userid = Meteor.userId();
+    self.subscribe('singleUser', userid);
+  });
+
+});
+
+
+Template.mainpage.helpers({
+  student: function(){
+    var id = Meteor.userId();
+    var user = Meteor.users.findOne({_id: id});
+    return user.userType.isStudent;
+  },
+  donor: function(){
+    var id = Meteor.userId();
+    var user = Meteor.users.findOne({_id: id});
+    return user.userType.isDonor;
+  },
+  uni: function(){
+    var id = Meteor.userId();
+    var user = Meteor.users.findOne({_id: id});
+    return user.userType.isUniAdmin;
+  },
+});
